8477905d81f859a45d7e40808147276f036d5089
Athanasios <zhdong2@gmail.com>
2017-01-09 18:37:08 -0800

    Update Chapter1-2.py

--- Chapter1/Chapter1-2.py
+++ Chapter1/Chapter1-2.py
@@ -1,8 +1,12 @@
 #Chapter 1.2 Data Type and Data Transformation
 # using int(), str(), float() can change the data to the type desired.
 pig3 = "2.85"
+
+#*************************************************************************************************************
 #Exercise 1.2 Search and describe the difference between float, integer and string variable.
 # What is the data type of the variable 'pig3'? Write a line of code to convert it to float.
+#*************************************************************************************************************
+
 
 #Beware you cannot add a float or an integer directly after a string like:
 # print("the number of pig is" + 3) <- you can try and see what error it creates...
@@ -18,6 +22,11 @@ fruit4 = "berries"
 
 salad = [fruit1,fruit2,fruit3,fruit4]
 print(salad) # also works for int and float, try that on your self
+
+#Important: Python counts list from 0 as the first... for example:
+
+print(salad[1])
+print(salad[-1]) #What does "-1" here means?
 #Combine data type??
 # Example: area variables (in square meters)
 hall = 6.25
@@ -28,6 +37,11 @@ bath = 11.50
 
 # Adapt list areas
 areas = [str("hallway"),hall, "kitchen", kit, str("living room"), liv, "bedroom",bed, str("bathroom"),bath]
+
+#*************************************************************************************************************
+# Exercise 1.3: Now calculate the sum of areas for the bedroom and bathroom for this house. It must be read as
+# "The sum area of bedroom and bathroom is 29.25 square meters".
+#*************************************************************************************************************
 
 # Print areas
 print (areas)
