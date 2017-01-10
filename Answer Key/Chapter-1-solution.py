93ff2238d0eacbe4bac7b9de344a308a119015a6
Athanasios <zhdong2@gmail.com>
2017-01-09 18:34:26 -0800

    Create Chapter-1-solution.py

---
+++ Answer Key/Chapter-1-solution.py
@@ -0,0 +1,20 @@
+#Answer Key
+#1.1
+print (100 * 1.02 ** 8)
+#1.2
+pig3 = "2.85"
+print(type(pig3))
+float(pig3)
+print(pig3)
+#1.3
+hall = 6.25
+kit = 11.0
+liv = 40.0
+bed = 17.75
+bath = 11.50
+
+# Adapt list areas
+areas = [str("hallway"),hall, "kitchen", kit, str("living room"), liv, "bedroom",bed, str("bathroom"),bath]
+
+#Still need to convert to string, but can actually do calculation within the string conversion... 
+print ("The sum area of bedroom and bathroom is "+ str(areas[-1]+areas[-3]) + " square meters.")
