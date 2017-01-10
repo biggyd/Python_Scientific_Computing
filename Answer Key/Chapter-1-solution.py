7e5376918c70f9006459367a6e209086d0554060
Athanasios <zhdong2@gmail.com>
2017-01-09 20:24:19 -0800

    Update Chapter-1-solution.py

--- Answer Key/Chapter-1-solution.py
+++ Answer Key/Chapter-1-solution.py
@@ -18,3 +18,26 @@ areas = [str("hallway"),hall, "kitchen", kit, str("living room"), liv, "bedroom"
 
 #Still need to convert to string, but can actually do calculation within the string conversion... 
 print ("The sum area of bedroom and bathroom is "+ str(areas[-1]+areas[-3]) + " square meters.")
+
+#1.4
+Jerry = 102
+Jason = 100
+Mary = "A"
+Molly = "A-"
+Martin = "B+"
+Shin = "C-"
+Shook = 65
+Foster = 51
+
+grades = ["Jerry", Jerry, "Jason", Jason, "Mary", Mary, "Molly", Molly, "Martin", Martin,
+          "Shin", Shin,"Shook", Shook, "Foster", Foster]
+print (grades)
+okstudent = grades[:12]
+print(okstudent)
+notokstudent = grades[12:]
+print(notokstudent)
+
+#1.5
+pig= "PiGgy"
+smallpig = pig.lower()
+print(smallpig)
